001acdca2861ceaa19a4efce6c9e86c014ecfe38
Yanze Wu <wuyanze0@gmail.com>
2019-07-06 02:36:57 -0400

    Add example folder  for sbm

---
+++ sbm-example/README.txt
@@ -0,0 +1,11 @@
+This folder contains some simulation examples and utilities for spin-boson models.
+
+Models:
+
+single-*: Test model with only one single mode;
+debye1-*: Model with Debye spectral density; \eta=0.25, \omega_c=0.5, T=1.0, number of modes = 100;
+
+Utilities:
+
+gensbm.py: Generate spin boson model with given parameters.
+runsbm.cmd, runsbm.sh: Run spin boson model using Ehrenfest dynamics with given filename prefix, step and timestep.
---
+++ sbm-example/runsbm.cmd
@@ -0,0 +1,11 @@
+@echo off
+REM run spin boson model using Ehrenfest dynamics
+REM Usage: runsbm [filename_prefix] [step/10000] (timestep)
+REM Please make sure you are under this foler
+set fprefix=sbm-example/%~1
+set nstep=%~2
+shift & shift
+if "%1"=="" (set timestep=1e-3) else (set timestep=%1)
+cd ..
+@echo on
+python -m pyqd --x0=@%fprefix%-x.txt --k0=@%fprefix%-p.txt --args=%fprefix%-model.txt --nstep=%nstep%0000 --dt=%timestep% --dstep=100 --model=sbm --m=1 --task=ehrenfest --obj=population --box=-10000,10000 --batch=0
